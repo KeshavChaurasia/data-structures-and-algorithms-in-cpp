a36d849b1126bcdf676cbd3157230bcef87d2914
Keshav Chaurasia <keshav.chaurasia@pdc01.lisnepal.com.np>
2019-12-05 17:31:03 +0545

    question 1 done

---
+++ chapter-1/question1.cpp
@@ -0,0 +1,32 @@
+// 1. Implement the simple methods getNum and getDen that will return the numerator and denominator of a fraction.
+#include<iostream>
+
+class Fraction{
+private:
+	int num;
+	int den;
+public:
+	Fraction(int num, int den){
+		num = num;
+		den = den;
+	}
+
+	Fraction(){
+		num = 1;
+		den = 1;
+	}
+
+	int getNum(){
+		return num;
+	}
+
+	int getDen(){
+		return den;
+	}
+}
+int main(){
+	Fraction frac(2,3);
+	std::cout << frac.getNum() << std::endl;
+	std::cout << frac.getDen() << std::endl;
+ 	return 0
+}
