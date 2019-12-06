1e4b7d49f31d591b566782ab6bb7a3c7d893e4d6
keshavchaurasia <keshav.chaurasia1995@gmail.com>
2019-12-06 21:23:14 +0545

    minus in denominator fixed

--- chapter-1/example6.cpp
+++ chapter-1/example6.cpp
@@ -1,47 +1,50 @@
-// Implement the remaining relational operators (>, >=, <, <=, and !=)
+// example 6
 //
 #include<iostream>
 using namespace std;
 int gcd(int a , int b){
-	while(a % b != 0){
-		int old_a = a;
-		int old_b = b;
+        while(a % b != 0){
+                int old_a = a;
+                int old_b = b;
 
-		a = old_b;
-		b = old_a % old_b;
-	}
-	return b;
+                a = old_b;
+                b = old_a % old_b;
+        }
+        return b;
 };
 class Fraction{
-	private:
-		int num;
-		int den;
-	public:
-		Fraction(int numerator, int denominator){
-			num = numerator;
-			den = denominator;
-		}
-
-		Fraction(){
-			num = 1;
-			den = 1;
-		}
-		friend Fraction operator + (const Fraction &frac, const Fraction &other_frac);
-		friend Fraction operator - (const Fraction &frac, const Fraction &other_frac);
-		friend Fraction operator * (const Fraction &frac, const Fraction &other_frac);
-		friend Fraction operator / (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator < (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator > (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator == (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator <= (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator >= (const Fraction &frac, const Fraction &other_frac);
-		friend bool operator != (const Fraction &frac, const Fraction &other_frac);
-		friend ostream &operator << (ostream &out, const Fraction &f);
+        private:
+                int num;
+                int den;
+        public:
+                Fraction(int numerator, int denominator){
+                        num = numerator;
+                        den = denominator;
+			if(denominator < 0){
+				num = -num;
+				den = -den;
+			}
+                }
+                Fraction(){
+                        num = 1;
+                        den = 1;
+                }
+                friend Fraction operator + (const Fraction &frac, const Fraction &other_frac);
+                friend Fraction operator - (const Fraction &frac, const Fraction &other_frac);
+                friend Fraction operator * (const Fraction &frac, const Fraction &other_frac);
+                friend Fraction operator / (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator < (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator > (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator == (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator <= (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator >= (const Fraction &frac, const Fraction &other_frac);
+                friend bool operator != (const Fraction &frac, const Fraction &other_frac);
+                friend ostream &operator << (ostream &out, const Fraction &f);
 };
 Fraction operator + (const Fraction &frac, const Fraction &other_frac){
-	int temp_num = frac.num*other_frac.den + other_frac.num*frac.den;
-	int temp_den = frac.den * other_frac.den;
-	return Fraction(temp_num,temp_den);
+        int temp_num = frac.num*other_frac.den + other_frac.num*frac.den;
+        int temp_den = frac.den * other_frac.den;
+        return Fraction(temp_num,temp_den);
 };
 Fraction operator - (const Fraction &frac, const Fraction &other_frac){
         int temp_num = frac.num*other_frac.den - other_frac.num*frac.den;
@@ -78,9 +81,13 @@ bool operator >= (const Fraction &frac, const Fraction &other_frac){
 };
 
 ostream &operator << (ostream &out, const Fraction &f){
-	int divisor = gcd(f.num,f.den);
+	int divisor = gcd(abs(f.num),abs(f.den));
 	int n = f.num/divisor;
 	int d = f.den/divisor;
+	if(d < 0){
+		d = -d;
+		n = -n;
+	}
 	if(n == 1 && d == 1){
 		out << 1 << endl;
 	}
@@ -88,18 +95,18 @@ ostream &operator << (ostream &out, const Fraction &f){
 		out << 0 << endl ;
 	}
 	else {
-		 out << f.num/divisor << "/" << f.den/divisor << endl;
+		out << f.num/divisor << "/" << f.den/divisor << endl;
 	}
 	return out;
 };
 
 int main(){
 	Fraction frac1(5,15);
-	Fraction frac2(10,15);
+	Fraction frac2(-10,15);
 	cout << "frac1: " << frac1 << endl;
 	cout << "frac2: " << frac2 << endl;
 	cout << "sum: " << frac1+frac2 << endl;
-	cout << "difference: " << frac2-frac1 << endl;
+	cout << "difference: " << frac1-frac2 << endl;
 	cout << "division: " << frac1/frac2 << endl;
 	cout << "=: " << (frac1 == frac2) << endl;
 	cout << "!=: " << (frac1 != frac2) << endl;
